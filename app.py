9abd0a9037313e900e3f6b1113627eafca22d5cd
vtur18 <112113791+vtur18@users.noreply.github.com>
2022-08-27 01:44:43 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import random
+import os
+import requests
+from flask import Flask, render_template, abort, request
+
+from meme import generate_meme
+from MemeEngine import MemeEngine
+from QuoteEngine import Ingestor
+
+app = Flask(__name__)
+
+meme = MemeEngine('./static')
+
+
+def setup():
+    """ Load all resources """
+
+    quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
+                   './_data/DogQuotes/DogQuotesDOCX.docx',
+                   './_data/DogQuotes/DogQuotesPDF.pdf',
+                   './_data/DogQuotes/DogQuotesCSV.csv']
+
+    quotes = []
+    for f in quote_files:
+        quotes.extend(Ingestor.parse(f))
+
+    images_path = "./_data/photos/dog/"
+
+    imgs = []
+    for root, dirs, files in os.walk(images_path):
+        imgs = [os.path.join(root, name) for name in files]
+
+    return quotes, imgs
+
+
+quotes, imgs = setup()
+
+
+@app.route('/')
+def meme_rand():
+    """ Generate a random meme """
+    img = random.choice(imgs)
+    quote = random.choice(quotes)
+    path = meme.make_meme(img, quote.body, quote.author)
+    return render_template('meme.html', path=path)
+
+
+@app.route('/create', methods=['GET'])
+def meme_form():
+    """ User input for meme information """
+    return render_template('meme_form.html')
+
+
+@app.route('/create', methods=['POST'])
+def meme_post():
+    """ Create a user defined meme """
+    try:
+        img = requests.get(request.form['image_url'])
+    except (requests.exceptions.HTTPError,
+            requests.exceptions.ConnectionError,
+            requests.exceptions.InvalidURL):
+        print('Please enter a valid URL')
+
+    b = request.form['body']
+    a = request.form['author']
+    tmp = f'./static/{random.randint(1, 100000)}.png'
+    open(tmp, 'wb').write(img.content)
+    path = meme.make_meme(tmp, b, a)
+    os.remove(tmp)
+    return render_template('meme.html', path=path)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ meme.py
@@ -0,0 +1,53 @@
+import os
+import random
+import argparse
+
+from MemeEngine import MemeEngine
+from QuoteEngine import QuoteModel, Ingestor
+
+
+def generate_meme(path=None, body=None, author=None):
+    """ Generate a meme given an path and a quote """
+    img = None
+    quote = None
+
+    if path is None:
+        images = "./_data/photos/dog/"
+        imgs = []
+        for root, dirs, files in os.walk(images):
+            imgs = [os.path.join(root, name) for name in files]
+
+        img = random.choice(imgs)
+    else:
+        img = path
+
+    if body is None:
+        quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
+                       './_data/DogQuotes/DogQuotesDOCX.docx',
+                       './_data/DogQuotes/DogQuotesPDF.pdf',
+                       './_data/DogQuotes/DogQuotesCSV.csv']
+        quotes = []
+        for f in quote_files:
+            quotes.extend(Ingestor.parse(f))
+
+        quote = random.choice(quotes)
+    else:
+        if author is None:
+            raise Exception('Author Required if Body is Used')
+        quote = QuoteModel(body, author)
+
+    meme = MemeEngine('./tmp')
+    path = meme.make_meme(img, quote.body, quote.author)
+    return path
+
+
+if __name__ == "__main__":
+    """Create ArgumentParser to parse CLI arguments."""
+
+    parser = argparse.ArgumentParser(description='create dog meme')
+    parser.add_argument('--path', type=str, default=None, help="Location of image.")
+    parser.add_argument('--body', type=str, default=None, help="Dog quotes")
+    parser.add_argument('--author', type=str, default=None, help="Author of the quote")
+    args = parser.parse_args()
+
+    print(generate_meme(args.path, args.body, args.author))
